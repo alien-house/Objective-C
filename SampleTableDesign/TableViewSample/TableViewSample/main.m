9f3a89c0d68c126078259e91674749393ed726d9
alien-house <info@alien-house.com>
2017-03-18 06:11:48 -0700

    SampleTableDesign

---
+++ SampleTableDesign/TableViewSample/TableViewSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewSample
+//
+//  Created by sin on 2017/03/16.
+//  Copyright © 2017年 shinji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
